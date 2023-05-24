3cb034dd61900f525c96c8dbbb10cbf5fafcd0ae
hotnuma <hotnuma@gmail.com>
2023-05-24 13:50:35 +0200

    meson.build : install desktop file

--- install.sh
+++ install.sh
@@ -11,18 +11,4 @@ meson build -Dbuildtype=plain
 ninja -C build
 sudo ninja -C build install
 
-# Install Launcher -------------------------------------------------------------
-
-dest=/usr/local/share/applications/
-if [[ ! -d $dest ]]; then
-    echo "*** create application directory"
-    sudo mkdir $dest
-fi
-
-dest=/usr/local/share/applications/
-if [[ ! -f ${dest}/fileman.desktop ]]; then
-    echo "*** install desktop file"
-    sudo cp $BASEDIR/data/fileman.desktop $dest
-fi
-
 
